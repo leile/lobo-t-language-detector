5d20cbdfb4bcda3284bc45ddd6d406533e249865
Hans Magnus Inderberg <hinderberg@gmail.com>
2019-12-04 14:06:33 +0100

    Return type as languages

--- src/path-lookup.ts
+++ src/path-lookup.ts
@@ -8,20 +8,20 @@ type ValueOf<T> = T[keyof T];
 
 type KeyType = string | number;
 
-export interface PathLookup {
-  getLanguageFromPath: (path: string) => KeyType;
+export interface PathLookup<Languages extends StandardEnum<KeyType>> {
+  getLanguageFromPath: (path: string) => ValueOf<Languages>;
 }
 
 export const createPathLookup = <Languages extends StandardEnum<KeyType>>(
   allowedLanguages: Array<ValueOf<Languages>>,
   defaultLanguage: ValueOf<Languages>
-): PathLookup => {
-  const getLanguageFromPath = (path: string): KeyType => {
+): PathLookup<Languages> => {
+  const getLanguageFromPath = (path: string): ValueOf<Languages> => {
     const languages = path.match(/\/([a-zA-Z-]*)/g);
     if (languages instanceof Array) {
-      const language = languages[0].replace('/', '');
+      const language = languages[0].replace('/', '') as ValueOf<Languages>;
 
-      if (allowedLanguages.includes(language as ValueOf<Languages>)) {
+      if (allowedLanguages.includes(language)) {
         return language;
       }
     }
